3046c0d9ef6d1ec027b316b8bf34a96c97a7c451
Mayk <7395296+y0k4i-1337@users.noreply.github.com>
2025-11-05 10:44:04 -0300

    Add script to setup corellium

--- corellium-setup.sh
+++ corellium-setup.sh
@@ -12,19 +12,22 @@ if [ -z "$DEVICE_IP" ]; then
     exit 1
 fi
 
+# Run script to get Burp certificate
+./getburpcert.sh
+
 CERTS_DIR="./certs"
 if [ ! -d "$CERTS_DIR" ]; then
-    echo "Certificates directory $CERTS_DIR does not exist. Please run getburpcert.sh first."
+    echo "[!] Certificates directory $CERTS_DIR does not exist. Please run getburpcert.sh first."
     exit 1
 fi
 
 CERT_PATH="$CERTS_DIR/$(ls $CERTS_DIR | grep '\.0$' | head -1)"
 if [ ! -f "$CERT_PATH" ]; then
-    echo "Certificate file not found in $CERTS_DIR. Please ensure getburpcert.sh has been run successfully."
+    echo "[!] Certificate file not found in $CERTS_DIR. Please ensure getburpcert.sh has been run successfully."
     exit 1
 fi
 
-echo "Setting up Corellium device at $DEVICE_IP for Burp Suite"
+echo "[*] Setting up Corellium device at $DEVICE_IP for Burp Suite"
 
 # Connect to the device and install the certificate
 adb connect "$DEVICE_IP:5001"
@@ -32,25 +35,25 @@ adb root
 adb push "$CERT_PATH" /system/etc/security/cacerts/
 
 # Verify installation
-echo "Verifying certificate installation on device"
+echo "[*] Verifying certificate installation on device"
 # Get certificate name
 filename=$(basename "$CERT_PATH")
-echo "Expected certificate filename on device: $filename"
+echo "[*] Expected certificate filename on device: $filename"
 adb shell "ls /system/etc/security/cacerts/$filename"
 
 # Setup proxy settings if $2 is provided
 BURP_PROXY="$2"
 if [ -z "$BURP_PROXY" ]; then
-    echo "No Burp proxy provided, skipping proxy setup."
+    echo "[-] No Burp proxy provided, skipping proxy setup."
     exit 0
 fi
 
-echo "Setting up proxy settings on device"
+echo "[*] Setting up proxy settings on device"
 adb shell settings put global http_proxy $BURP_PROXY
 
-echo "Verifying proxy settings on device"
+echo "[*] Verifying proxy settings on device"
 adb shell settings get global http_proxy
 
-echo "Corellium device setup complete."
-echo "To remove the proxy settings, run:"
+echo "[*] Corellium device setup complete.\n"
+echo "[*] To remove the proxy settings, run:"
 echo "adb shell settings put global http_proxy :0"
